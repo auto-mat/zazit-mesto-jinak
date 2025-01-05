2aac8285f1541da54d0962f4434919667569def2
Jana Schořová <janca.schorova@seznam.cz>
2025-01-05 12:33:19 +0100

    Add new font

--- quasar.config.ts
+++ quasar.config.ts
@@ -35,7 +35,6 @@ export default configure((/* ctx */) => {
       // 'line-awesome',
       // 'roboto-font-latin-ext', // this or either 'roboto-font', NEVER both!
 
-      'roboto-font', // optional, you are not bound to it
       'material-icons', // optional, you are not bound to it
     ],
 
