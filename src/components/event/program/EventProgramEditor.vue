33cf28ee2b0a5469ae1a3a8b6c450c5aef675cdc
Jana Schořová <janca.schorova@seznam.cz>
2025-03-28 10:04:03 +0100

    Add program editor component

---
+++ src/components/event/program/EventProgramEditor.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <!-- table: editable -->
+    <div class="q-mb-xl">
+      <h2 class="q-mt-0 q-mb-md text-body1 text-weight-bold">
+        {{ $t('event.program.titleExistingProgramItems') }}
+      </h2>
+
+      <event-program-table editable />
+    </div>
+
+    <!-- form: program item -->
+    <event-program-item-form />
+  </div>
+</template>
+
+<script setup lang="ts">
+import EventProgramTable from './EventProgramTable.vue';
+import EventProgramItemForm from './EventProgramItemForm.vue';
+
+</script>
