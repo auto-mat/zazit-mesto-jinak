a8343d13e8b042b374b4a2a6fb822d1ab35d442b
Jana Schořová <janca.schorova@seznam.cz>
2025-12-16 11:26:29 +0100

    Fix marker

--- src/components/global/LeafletMap.vue
+++ src/components/global/LeafletMap.vue
@@ -1,28 +1,31 @@
 <template>
-   <div id="map"></div>
+  <div id="map"></div>
 </template>
 
 <script setup lang="ts">
 import { computed, onMounted } from 'vue';
 import leaflet from 'leaflet';
 
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
 const props = defineProps({
-   latitude: {
-      type: Number,
-      default: 50.073658, // Prague
-   },
-   longitude: {
-      type: Number,
-      default: 14.418540, // Prague
-   },
-   marker: {
-      type: Boolean,
-      default: false,
-   },
-   editable: {
-      type: Boolean,
-      default: false,
-   }
+  latitude: {
+    type: Number,
+    default: 50.073658, // Prague
+  },
+  longitude: {
+    type: Number,
+    default: 14.41854, // Prague
+  },
+  marker: {
+    type: Boolean,
+    default: false,
+  },
+  editable: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 const emits = defineEmits(['setCoords']);
@@ -30,33 +33,44 @@ const emits = defineEmits(['setCoords']);
 let map: leaflet.Map;
 let eventMarker: leaflet.Marker;
 
-const zoom = computed(() => props.marker ? 17 : 12)
+const zoom = computed(() => (props.marker ? 17 : 12));
+
+let defaultIcon = leaflet.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+});
 
 onMounted(() => {
-   map = leaflet.map('map').setView([props.latitude, props.longitude], zoom.value)
+  leaflet.Marker.prototype.options.icon = defaultIcon;
 
-   leaflet.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  map = leaflet
+    .map('map')
+    .setView([props.latitude, props.longitude], zoom.value);
+
+  leaflet
+    .tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-   }).addTo(map);
+      attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    })
+    .addTo(map);
 
-   if (eventMarker) map.removeLayer(eventMarker);
+  if (eventMarker) map.removeLayer(eventMarker);
 
-   if (props.marker) {
-      eventMarker = leaflet.marker([props.latitude, props.longitude]).addTo(map);
-   }
+  if (props.marker) {
+    eventMarker = leaflet.marker([props.latitude, props.longitude]).addTo(map);
+  }
 
-   if (props.editable) {
-      map.addEventListener('click', (e) => {
-         if (eventMarker) map.removeLayer(eventMarker);
-      
-         const { lat, lng } = e.latlng;
-         eventMarker = leaflet.marker([lat, lng]).addTo(map);
-         emits('setCoords', lat, lng);
-      })
-   }
-})
+  if (props.editable) {
+    map.addEventListener('click', (e) => {
+      if (eventMarker) map.removeLayer(eventMarker);
+
+      const { lat, lng } = e.latlng;
+      eventMarker = leaflet.marker([lat, lng]).addTo(map);
+      emits('setCoords', lat, lng);
+    });
+  }
+});
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
