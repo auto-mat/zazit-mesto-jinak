9d85afff1cfd32f0f76297ed47cdca1f6eddda46
Jana Schořová <janca.schorova@seznam.cz>
2025-12-21 01:11:05 +0100

    Refactor content pages

--- src/pages/event/content/EventContentEditPage.vue
+++ src/pages/event/content/EventContentEditPage.vue
@@ -1,14 +1,11 @@
 <template>
   <q-page class="column q-pa-xl">
-    <div v-if="isLoading" class="loading">Loading...</div>
-
-    <!-- <div v-if="error" class="error">{{ error }}</div> -->
-
-    <div v-if="eventContent && eventProgram" class="content">
+    <div v-if="isEventContentLoading" class="loading">Loading...</div>
+    <div v-else class="content">
       <div class="row justify-between items-end q-mb-md">
         <div>
           <span>{{ eventName }}</span>
-          <h1>{{ t('event.titleInformation') }}</h1>
+          <h1>{{ t('event.titleWebContent') }}</h1>
         </div>
 
         <back-button
@@ -19,48 +16,86 @@
         />
       </div>
 
-      <!-- <embed 
-        src="https://zazitmestojinak.cz/locations_/praha/praha-3/husitska" 
-        style="width:500px; height: 300px;"
-      > -->
-
-      <event-content-editor :event-content @save="onSave" />
+      <div class="q-pt-md q-pb-xl">
+        <form-event-content @save="onSave" @reset="onBack" />
+      </div>
     </div>
+    <discard-changes-modal
+      v-model="discardChangesModal"
+      :is-saving="isEventContentSaving"
+      @cancel="discardChangesModal = false"
+      @discard-changes="discardChanges"
+      @save="onSave"
+    />
   </q-page>
 </template>
 
 <script setup lang="ts">
-import { computed, onMounted, ref } from 'vue';
+// libraries
+import { computed, ref, watch } from 'vue';
+import { storeToRefs } from 'pinia';
 import { useRoute, useRouter } from 'vue-router';
+import { useI18n } from 'vue-i18n';
+
+// config
 import { routesConf } from 'src/router/routes_conf';
 
+// components
 import BackButton from 'src/components/buttons/BackButton.vue';
-import EventContentEditor from 'src/components/event/content/EventContentEditor.vue';
+import FormEventContent from 'src/components/form/event/FormEventContent.vue';
+
+// stores
 import { useEventStore } from 'src/stores/event';
-import { useI18n } from 'vue-i18n';
+import { useEventContentStore } from 'src/stores/event/content';
 
 const { t } = useI18n();
 
 const route = useRoute();
 const slug = ref(route.params.slug as string);
-
-const eventStore = useEventStore();
 const router = useRouter();
 
-const onSave = (): void => {
+const eventStore = useEventStore();
+const eventContentStore = useEventContentStore();
+const { isEventContentLoading, isEventContentFormDirty, isEventContentSaving } =
+  storeToRefs(eventContentStore);
+const discardChangesModal = ref(false);
+
+const discardChanges = (): void => {
+  eventContentStore.resetEventContentForm();
   router.push({
     name: routesConf['event_web_content']['children']['name'],
-    params: { slug: slug.value },
+    params: { slug: slug.value as string },
   });
 };
 
-onMounted(() => {
-  eventStore.loadEventContent(slug.value);
-  eventStore.loadEventProgram(slug.value);
-});
+const onSave = async (): Promise<void> => {
+  const success = await eventContentStore.updateEventContent();
+  if (success) {
+    router.push({
+      name: routesConf['event_web_content']['children']['name'],
+      params: { slug: slug.value },
+    });
+  }
+};
 
-const isLoading = computed(() => eventStore.isLoading);
+const onBack = (): void => {
+  if (isEventContentFormDirty.value) {
+    discardChangesModal.value = true;
+  } else {
+    router.push({
+      name: routesConf['event_web_content']['children']['name'],
+      params: { slug: slug.value },
+    });
+  }
+};
+
 const eventName = computed(() => eventStore.getEventName(slug.value));
-const eventContent = computed(() => eventStore.eventContent);
-const eventProgram = computed(() => eventStore.eventProgram);
+
+watch(
+  slug,
+  () => {
+    eventStore.setSlug(slug.value as string);
+  },
+  { immediate: true },
+);
 </script>
---
+++ src/stores/event/content.ts
@@ -0,0 +1,118 @@
+import { defineStore } from 'pinia';
+import { ref, watch } from 'vue';
+import { EventContent, EventContentForm } from 'src/types/Event';
+
+import { useApiEventContent } from 'src/composables/api/event/useApiEventContent';
+import { cloneDeep, pick } from 'lodash';
+import { useEventStore } from '../event';
+import { Notify } from 'quasar';
+import { i18n } from 'src/boot/i18n';
+
+export const useEventContentStore = defineStore('eventsContent', () => {
+  const eventStore = useEventStore();
+  const { getEventContentApi, updateEventContentApi } = useApiEventContent();
+
+  // Event content
+  const eventContent = ref<EventContent | null>(null);
+  const isEventContentLoading = ref<boolean>(false);
+
+  const loadEventContent = async (slug: string): Promise<void> => {
+    isEventContentLoading.value = true;
+    const eventContentResponse = await getEventContentApi(slug);
+
+    if (!eventContentResponse) {
+      return;
+    }
+
+    eventContent.value = eventContentResponse;
+
+    isEventContentLoading.value = false;
+  };
+
+  // Event content form
+  const defaultEventContentForm: EventContentForm = {
+    image: null,
+    mainContent: '',
+    links: [],
+  };
+
+  const eventContentForm = ref<EventContentForm>(
+    cloneDeep(defaultEventContentForm),
+  );
+  const isEventContentFormDirty = ref(false);
+
+  const clearEventContentForm = (): void => {
+    eventContentForm.value = cloneDeep(defaultEventContentForm);
+    isEventContentFormDirty.value = false;
+  };
+
+  const resetEventContentForm = (): void => {
+    eventContentForm.value = cloneDeep(eventContent.value);
+    isEventContentFormDirty.value = false;
+  };
+
+  watch(eventContent, (newEventContent) => {
+    if (newEventContent) {
+      eventContentForm.value = cloneDeep(
+        pick(newEventContent, ['mainContent', 'links']),
+      );
+      isEventContentFormDirty.value = false;
+    }
+  });
+
+  // Update event content
+  const isEventContentSaving = ref<boolean>(false);
+
+  const updateEventContent = async (): Promise<boolean> => {
+    if (isEventContentSaving.value) {
+      return false;
+    }
+    if (!eventStore.slug) {
+      return false;
+    }
+    isEventContentSaving.value = true;
+    const success = await updateEventContentApi(
+      eventStore.slug,
+      eventContentForm.value,
+    );
+    if (success) {
+      clearEventContentForm();
+      await loadEventContent(eventStore.slug);
+      Notify.create({
+        message: i18n.global.t('event.content.successUpdate'),
+        color: 'positive',
+      });
+      isEventContentSaving.value = false;
+      return true;
+    }
+    isEventContentSaving.value = false;
+    return false;
+  };
+
+  // Watch change of event
+  watch(
+    () => eventStore.slug,
+    async (newSlug) => {
+      if (newSlug) {
+        clearEventContentForm();
+        await loadEventContent(newSlug);
+      }
+    },
+  );
+
+  const clearEventContentStore = (): void => {
+    eventContent.value = null;
+  };
+
+  return {
+    eventContent,
+    eventContentForm,
+    isEventContentFormDirty,
+    isEventContentSaving,
+    isEventContentLoading,
+    loadEventContent,
+    updateEventContent,
+    clearEventContentStore,
+    resetEventContentForm,
+  };
+});
